7241e1af466b9dccdbc079baea1359d14ac40a1e
tshabi16 <tshepangrethabiletshabalala@gmail.com>
2025-08-16 01:03:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Highlight active nav link
 document.addEventListener("DOMContentLoaded", () => {
   const current = location.pathname.split("/").pop();
   document.querySelectorAll("nav a").forEach(link => {
-    if (link.getAttribute("href") === current) {
+    if(link.getAttribute("href") === current) {
       link.style.color = "#00ffcc";
     }
   });
